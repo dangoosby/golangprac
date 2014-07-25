91de06cde755c78c61b13022b730b8b14c8eabd2
Dan Goosby <daniel@macross.sx>
2014-07-24 23:38:50 -0400

    prac11

---
+++ pass00/prac11.go
@@ -0,0 +1,16 @@
+// golang practice 11 - functions
+
+package main 
+
+import "fmt"
+
+func plus(a int, b int) int {
+
+	return a + b
+}
+
+func main() {
+
+	res := plus(1, 2)
+	fmt.Println("1+2 =", res)
+}
