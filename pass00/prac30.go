60bce8e4f6c77e7d98629d0412029b98a0d9513f
Daniel <daniel.goosby@gmail.com>
2014-08-10 18:17:26 -0400

    prac30

---
+++ pass00/prac30.go
@@ -0,0 +1,17 @@
+// golang practice 30 - Range Over Channels
+
+package main
+
+import "fmt"
+
+func main() {
+
+    queue := make(chan string, 2)
+    queue <- "one"
+    queue <- "two"
+    close(queue)
+
+    for elem := range queue {
+        fmt.Println(elem)
+    }
+}
