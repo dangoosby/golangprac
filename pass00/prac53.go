7929c79e943100b63aa68a6db11cbfb28b1745a9
Daniel <daniel.goosby@gmail.com>
2014-08-14 15:21:29 -0400

    prac53

---
+++ pass00/prac53.go
@@ -0,0 +1,21 @@
+// golang practice 53 - SHA1 Hashes
+
+package main
+
+import "crypto/sha1"
+import "fmt"
+
+func main() {
+    s := "sha1 this string"
+
+    h := sha1.New()
+
+
+    h.Write([]byte(s))
+
+    bs := h.Sum(nil)
+
+
+    fmt.Println(s)
+    fmt.Printf("%x\n", bs)
+}
