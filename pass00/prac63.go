18f0ec71bedc30cabb942bd1696d6912ac29ecd9
Daniel <daniel.goosby@gmail.com>
2014-08-20 11:41:06 -0400

    prac63

---
+++ pass00/prac63.go
@@ -0,0 +1,27 @@
+// golang practice 63 - Signals
+
+package main
+
+import "fmt"
+import "os"
+import "os/signal"
+import "syscall"
+
+func main() {
+
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+}
