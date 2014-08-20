69297892aa7539ffa2048b0ece1e8839c67f3ca3
Daniel <daniel.goosby@gmail.com>
2014-08-20 11:30:25 -0400

    prac62

---
+++ pass00/prac62.go
@@ -0,0 +1,24 @@
+// golang practice 62 - Exec'ing Processes
+
+package main
+
+import "syscall"
+import "os"
+import "os/exec"
+
+func main() {
+
+	binary, lookErr := exec.LookPath("ls")
+	if lookErr != nil {
+		panic(lookErr)
+	}
+
+	args := []string{"ls", "-a", "-l", "-h"}
+
+	env := os.Environ()
+
+	execErr := syscall.Exec(binary, args, env)
+	if execErr != nil {
+		panic(execErr)
+	}
+}
