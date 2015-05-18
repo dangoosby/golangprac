e192a2d464db2c33fbb9338c5bcc29a89570e892
laughingstorm <daniel@macross.io>
2015-05-18 18:33:57 -0400

    prac03-14

---
+++ pass01/prac12.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func main() {
+	fmt.Println(strconv.FormatInt(time.Now().Unix(), 10))
+	fmt.Println(time.Now().Format(time.RFC850))
+	fmt.Println(time.Now().Format(time.RFC1123Z))
+	fmt.Println(time.Now().Format("20060102150405"))
+	fmt.Println(time.Unix(1401403874, 0).Format("02.01.2006 15:04:05"))
+	fmt.Println(time.Unix(123456789, 0).Format(time.RFC3339))
+	fmt.Println(time.Unix(1234567890, 0).Format(time.RFC822))
+}
